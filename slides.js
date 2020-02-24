5ed844f5a6a7926b5c3f6f2fb68c1348ecaef27a
Joan Marie Verba <verba001@umn.edu>
2020-02-24 13:48:15 -0600

    add code

--- slides.js
+++ slides.js
@@ -83,3 +83,12 @@ function showDivs(n) {
     x[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " w3-white";
 }
+
+
+//popup code
+
+window.onbeforeunload = popup;
+
+function popup() {
+    return 'I see you are leaving the site';
+}
